8592bad4d4113110dfe8d9d94aad6fa06eccb746
Yashvardhan Gupta <123872418+yashvardhann15@users.noreply.github.com>
2024-03-21 11:49:36 +0530

    weather.js

--- weather.js
+++ weather.js
@@ -1,4 +1,4 @@
-const apiKey = "add your own apiId. get it through opwnweathermap.org";
+const apiKey = process.env.API_KEY;
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
